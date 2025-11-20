c9061d611981fee35ff32e7e50a1e748a128aee9
hubbaka <hubbaka.ghoyati123@gmail.com>
2025-11-20 18:53:23 +0700

    Update metadata title and description; add custom favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Dribbble Invitation",
-  description: "An invitation for a designer to share their work on Dribbble",
+  title: "Secreat banget ini mah",
+  description: "screat pokonya hahaha",
 };
 
 export const viewport = {
@@ -32,6 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥰</text></svg>" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body
